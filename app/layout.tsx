41c61a17b8ab329bbe1c6f729ec7f4a9c35159db
홍범선 <qjatjs123123@naver.com>
2025-10-19 02:48:53 +0900

    fix : 배포 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import App from "../src/app/index";
 import "../src/app/styles/globals.css";
 
-if (
-  process.env.NEXT_RUNTIME === "nodejs" 
-) {
+if (typeof window === "undefined") {
   (async () => {
     const { server } = await import("@/mocks/server");
     server.listen({
