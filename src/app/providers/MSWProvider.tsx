ce598dade9eee46903202691aceef74b9fefdfe8
홍범선 <qjatjs123123@naver.com>
2025-10-25 14:37:30 +0900

    fix: 빌드 오류 해결

--- src/app/providers/MSWProvider.tsx
+++ src/app/providers/MSWProvider.tsx
@@ -1,48 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
-import { Suspense, use } from 'react'
-import { blogHandlers } from '@/entities/blog/api/__mocks__/get-blog-list-handler';
+import { Suspense, use } from 'react';
 
-const mockingEnabledPromise =
-  typeof window !== 'undefined'
-    ? import('@/mocks/browser').then(async ({  worker }) => {
-      if (process.env.NODE_ENV === 'production') {
-        return;
-      }
-      await worker.start({
-        onUnhandledRequest(request: { url: string | string[]; }, print: { warning: () => void; }) {
-          if (request.url.includes('_next')) {
-            return
-          }
-          print.warning()
-        },
-      })
-      worker.use(...blogHandlers);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (module as any).hot?.dispose(() => { worker.stop(); });
-      console.log(worker.listHandlers())
-    })
-    : Promise.resolve()
+let workerPromise: Promise<any> | null = null;
 
-export function MSWProvider({
-                              children,
-                            }: Readonly<{
-  children: React.ReactNode
-}>) {
-  // If MSW is enabled, we need to wait for the worker to start,
-  // so we wrap the children in a Suspense boundary until it's ready.
+if (typeof window !== 'undefined') {
+  workerPromise = import('@/mocks/browser').then(async ({ worker }) => {
+    await worker.start({
+      onUnhandledRequest(request, print) {
+        if (request.url.includes('_next')) return;
+        print.warning();
+      },
+    });
+
+    // HMR 시 Worker 중지
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (module as any).hot?.dispose(() => worker.stop());
+
+    console.log('MSW Handlers:', worker.listHandlers());
+  });
+}
+
+export function MSWProvider({ children }: { children: React.ReactNode }) {
   return (
     <Suspense fallback={null}>
       <MSWProviderWrapper>{children}</MSWProviderWrapper>
     </Suspense>
-  )
+  );
 }
 
-function MSWProviderWrapper({
-                              children,
-                            }: Readonly<{
-  children: React.ReactNode
-}>) {
-  use(mockingEnabledPromise)
-  return children
+function MSWProviderWrapper({ children }: { children: React.ReactNode }) {
+  use(workerPromise ?? Promise.resolve());
+  return <>{children}</>;
 }
