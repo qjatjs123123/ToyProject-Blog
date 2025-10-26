c88a84579930c690269d9e4246397be18633371d
홍범선 <qjatjs123123@naver.com>
2025-10-26 13:49:46 +0900

    refactor: 로그인 성공 콜백 리팩토링

--- src/features/sign-in/login/model/useLogin.ts
+++ src/features/sign-in/login/model/useLogin.ts
@@ -1,15 +1,31 @@
-import { postSignInForm, SignInFormProps } from "@/entities/sign-in";
+import { LoginToken, postSignInForm, SignInFormProps } from "@/entities/sign-in";
+import { toastMessage } from "@/shared/ui/Toast/model/action";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSetAtom } from "jotai";
+import { useRouter } from "next/navigation";
 import { useFormContext } from "react-hook-form";
 
 export function useLogin() {
   const queryClient = useQueryClient();
   const { getValues } = useFormContext<SignInFormProps>();
+  const setToastMessage = useSetAtom(toastMessage);
+  const router = useRouter();
   
   return useMutation({
     mutationFn: async () =>{
       const allValues = getValues();
-      await postSignInForm(allValues)
+      return await postSignInForm(allValues)
     },
+
+    onSuccess: (data: LoginToken) => {
+      setToastMessage("로그인 성공했어요");
+      queryClient.setQueryData(["accessToken"], data.accessToken);
+      localStorage.setItem("refreshToken", data.refreshToken);
+      router.push("/");
+    },
+
+    onError: (error: unknown) => {
+      setToastMessage("로그인 실패했어요");
+    }
   });
 }
