6bef39e2dfc59bb8c684acb08be856a29d83d053
홍범선 <qjatjs123123@naver.com>
2025-11-10 20:27:45 +0900

    test: 검색 e2e 테스트 코드 작성

--- cypress/e2e/2_blogs/blog.cy.ts
+++ cypress/e2e/2_blogs/blog.cy.ts
@@ -4,7 +4,7 @@ describe("블로그 페이지", () => {
     cy.visit("/blogs");
 
     // then 특정 블로그 카드 클릭 (예: id가 1인 카드)
-    cy.get('[data-cy="1"]')  // data-cy 값은 테스트용 id로 변경
+    cy.get('[data-cy="1"]') // data-cy 값은 테스트용 id로 변경
       .should("be.visible")
       .click();
     cy.wait(10000);
@@ -13,17 +13,31 @@ describe("블로그 페이지", () => {
     cy.url().should("include", "http://localhost:3000/blogs/1");
   });
 
-  it("카테고리를 클릭하면 URL이 변경된다.", () => {
+  it("사용자가 카테고리를 클릭하면 URL이 변경된다.", () => {
     // given 블로그 리스트 페이지 방문한다.
     cy.visit("/blogs");
 
     // then 트렌드 카테고리 클릭한다.
-    cy.get('[data-cy="트렌드"]')  // data-cy 값은 테스트용 id로 변경
+    cy.get('[data-cy="트렌드"]') // data-cy 값은 테스트용 id로 변경
       .should("be.visible")
       .click();
     cy.wait(2000);
 
     // then 쿼리 파라미터 URL 변경된다.
     cy.url().should("include", "http://localhost:3000/blogs?category=TREND");
+  });
+
+  it("사용자가 키워드로 블로그를 검색하면 URL이 변경된다.", () => {
+    // given 블로그 리스트 페이지 방문한다.
+    cy.visit("/blogs");
+
+    // then 뉴스를 입력후 엔터를 클릭한다.
+    cy.get('input[placeholder="입력해주세요"]') // 테스트용 data-cy로 변경 가능
+      .should("be.visible")
+      .clear()
+      .type("news{enter}");
+
+    // then 쿼리 파라미터 URL 변경된다.
+    cy.url().should("include", "http://localhost:3000/blogs?term=news");
   });
 });
