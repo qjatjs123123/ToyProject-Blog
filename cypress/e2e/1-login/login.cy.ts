30382d9fb3139ab1c5dae6815ce6c73b47b34a58
홍범선 <qjatjs123123@naver.com>
2025-11-10 18:23:06 +0900

    test: 로그인 성공 e2e 테스트

--- cypress/e2e/1-login/login.cy.ts
+++ cypress/e2e/1-login/login.cy.ts
@@ -7,17 +7,24 @@ describe("로그인 화면", () => {
   it("사용자는 아이디와 비밀번호를 사용해서 로그인한다", () => {
     // given - 로그인 페이지에 접근한다
     cy.visit("/sign-in");
-    cy.get(`input[placeholder="${BUSINESS_ID.placeholder}"]`).as("emailInput");
-    cy.get(`input[placeholder="${PASSWORD.placeholder}"]`).as("passwordInput");
 
-    // when - 아아디와 비밀번호를 입력하고 로그인 버튼을 클릭한다
-    cy.get("@emailInput").type("1234567890");
-    cy.get("@passwordInput").type("password!@");
+    cy.get(`input[placeholder="${BUSINESS_ID.placeholder}"]`)
+      .should("be.visible")
+      .as("emailInput");
+
+    cy.get(`input[placeholder="${PASSWORD.placeholder}"]`)
+      .should("be.visible")
+      .as("passwordInput");
+
+    // when - 아이디와 비밀번호 입력한다
+    cy.get("@emailInput").clear().type("1234567890");
+    cy.get("@passwordInput").clear().type("password!@");
 
     cy.get("@emailInput").invoke("val").should("eq", "1234567890");
+    cy.get("@passwordInput").invoke("val").should("eq", "password!@");
 
-    // then - 로그인에 성공하고 메인화면으로 이동한다
-    cy.get('[data-cy="login-button"]').click();
-    cy.url().should('include', 'http://localhost:3000')
+    // then - 로그인 버튼 클릭 후 URL 변경 된다.
+    cy.get('[data-cy="login-button"]').should("be.enabled").click();
+    cy.url().should("include", "http://localhost:3000/blogs");
   });
 });
