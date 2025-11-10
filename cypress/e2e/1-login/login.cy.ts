3c6655f99845df729b0c2e5fdc3776a83b306f1b
홍범선 <qjatjs123123@naver.com>
2025-11-10 19:14:39 +0900

    test: 로그인 데이터 형식  에러 e2e 테스트

--- cypress/e2e/1-login/login.cy.ts
+++ cypress/e2e/1-login/login.cy.ts
@@ -28,7 +28,7 @@ describe("로그인 화면", () => {
     cy.url().should("include", "http://localhost:3000/blogs");
   });
 
-  it("사용자는 계정 정보가 없는 아이디로 로그인 시도시 토스트 바에 뜬다.", () => {
+  it("사용자는 유효성 맞지 않는 아이디, 비밀번호로 로그인 시도시 토스트 바에 뜬다.", () => {
     // given - 로그인 페이지에 접근한다
     cy.visit("/sign-in");
 
@@ -52,5 +52,31 @@ describe("로그인 화면", () => {
     cy.get('[data-cy="toast"]') 
       .should("be.visible")
       .and("contain.text", "데이터 형식이 올바르지 않습니다.");
+  });
+
+  it("사용자는 계정 정보가 없는 아이디로 로그인 시도시 토스트 바에 뜬다.", () => {
+    // given - 로그인 페이지에 접근한다
+    cy.visit("/sign-in");
+
+    cy.get(`input[placeholder="${BUSINESS_ID.placeholder}"]`)
+      .should("be.visible")
+      .as("emailInput");
+
+    cy.get(`input[placeholder="${PASSWORD.placeholder}"]`)
+      .should("be.visible")
+      .as("passwordInput");
+
+    // when - 아이디와 비밀번호 입력한다 (invalid)
+    cy.get("@emailInput").clear().type("1234567890");
+    cy.get("@passwordInput").clear().type("wrongPassword");
+
+    cy.get("@emailInput").invoke("val").should("eq", "1234567890");
+    cy.get("@passwordInput").invoke("val").should("eq", "wrongPassword");
+
+    // then - 로그인 버튼 클릭 후 토스트창이 뜬다.
+    cy.get('[data-cy="login-button"]').should("be.enabled").click();
+    cy.get('[data-cy="toast"]') 
+      .should("be.visible")
+      .and("contain.text", "계정 정보를 다시 확인해주세요.");
   });
 });
